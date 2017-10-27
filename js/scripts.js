147ca8ba966a319d029c9ed7e0ae3056c4169a5f
Amy Churchwell <amy.churchwell@gmail.com>
2017-10-27 10:50:21 -0700

    create function for divisible by 3 spec add bootstrap styles to index

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-//back-end//
+// back-end magic
 
-//range of numbers//
+// 1. It can count up to the provided number.
 var countUp = function(input){
   var answer = [];
   for(var i = 1; i < input; i++) {
@@ -10,22 +10,24 @@ var countUp = function(input){
 }
 
 
-//replaces 0's and 1's, needs to replace entire number.//
+// 2. Numbers that *contain* a 1 are replaced with "Boop!"
+// 3. Numbers that *contain* a 0 are replaced with "Beep!"
 var beepBoop = function(input){
   return input.replace(/[1]/g, "Boop!").replace(/[0]/g, "Beep!");
 }
 
-//4. Numbers _divisible by 3_ are replaced with "I'm sorry, Dave. I'm afraid I can't do that."//
+// 4. Numbers _divisible by 3_ are replaced with 'I'm sorry, Dave. I'm afraid I can't do that.'
  var divide = function(input){
-   if(input % 3){
-     return "I'm sorry, Dave. I'm afraid I can't do that.";
+   if(input % 3 === 0){
+     answer = "I'm sorry, Dave. I'm afraid I can't do that."
+     return answer;
    }else{
-     return "TEST";
+     return countUp(input);
    }
  }
 
 
-//front-end//
+// front-end
 
 $(document).ready(function(){
   $('#form').submit(function(event){
